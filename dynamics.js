0e0dd31a497e4d9869bbad355fcb72cbb79f8072
gigatesseract <neevarp.star@gmail.com>
2018-05-15 08:54:44 +0530

    INtermediate1

--- dynamics.js
+++ dynamics.js
@@ -1,45 +1,56 @@
-var mentees = [];
-
-
-// function refresh() {
-//
-//   var list = document.getElementById('list');
-//     var length = localStorage.getItem("length");
-//
-//   for (i = 0; i < length; i++) {
-//     var len = i.toString(10);
-//   JSON.parse(localStorage.getItem(len));
-//   var div = JSON.parse(localStorage.getItem(len));
-//
-//     list.appendChild(div);
-//   }
-//
-//
-//
-//
-// }
-// if(localStorage.getItem("flag")=="1")
-// refresh();
+var mentees = []; //mentees and rating array are for keeping track of tree configuration for sorting
+var objarray = [];
 var rating = [];
-var j = 0,
-  i;
-
-function add() {
+var j = 0, i;
 
 
+document.getElementById('add').addEventListener("click",function(){ add(document.getElementById('text').value, 1, false, "")});
+
+function reconstruct() {
+var length = localStorage.getItem("length");
+  objarray.splice(0,length);
+
+
+  for (i = 0; i < length; i++) {
+  var obj = JSON.parse(localStorage.getItem(i));
+  console.log(JSON.parse(localStorage.getItem(i)));
+  add(obj.name, obj.rat, obj.lockstatus, obj.comments);
+
+
+  }
+
+
+
+
+}
+
+
+window.addEventListener("load", reconstruct);
+
+function add(name, ratings, lockstatus, comments) {                //START OF ADD FUNCTION
+
+    var list = document.getElementById('list'); //getting the reference to ul
+
+ var myobj = new Object();
   var div = document.createElement('div'); //creating a div element everytime add is clicked
 
-  var node = document.createElement('input'); //reference to li that will hold the name
+  var node = document.createElement('input'); //reference to input that will hold the name
   node.type = "text";
   node.placeholder = "Enter name here";
   node.className = "mentee";
-  node.value = document.getElementById('text').value; //setting the content of li to user input
-  var list = document.getElementById('list'); //getting the reference to ul
-  var button = document.createElement('button'); //Deletebutton
-  button.textContent = "Delete";
+  node.value = name; //setting the content of name from argument
+
+
+  var deletebutton = document.createElement('input'); //DELETEBUTTON PROPERTIES
+  deletebutton.type  = "button";
+  deletebutton.value = "Delete";
+  deletebutton.className = "delete"
   j++;
 
 
+  localStorage.setItem("j", j);
+
+  //SETTING THE RADIO BUTTONS AND THEIR CORRESPONDING LABELS
 
   var i;
   var radio = new Array(5);
@@ -48,7 +59,7 @@ function add() {
     radio[i] = document.createElement('input');
     radio[i].type = "radio";
     radio[i].className = "radio" + i;
-    radio[i].name = "radio" + j;
+    radio[i].name = "radio" + localStorage.getItem("j");
     var val = i + 1;
     radio[i].value = val.toString(10);
     label[i] = document.createElement('label');
@@ -57,32 +68,40 @@ function add() {
 
 
   }
+  var pos = parseInt(ratings);
+  pos = pos-1;
+  radio[pos].checked = true;
+  func(pos+1);
 
 
-  var lockbutton = document.createElement('input');
+  var lockbutton = document.createElement('input');       //LOCK BUTTON PROPERTIES
   lockbutton.type = "button";
   lockbutton.value = "Lock";
   lockbutton.className = "lock";
 
 
-
-  var para = document.createElement('p');
+ var para = document.createElement('p');           //RATING p PROPERTIES
   para.textContent = "Specify rating:- ";
-  para.className = "rating"; //para:- the rating heading
-  var comments = document.createElement('textarea');
-  comments.type = "text";
-  comments.placeholder = "Enter your comments here";
-  comments.className = "comments";
+  para.className = "rating";
 
-  var editbutton = document.createElement('input');
+
+  var comment = document.createElement('input');     //COMMENT PROPERTIES
+  comment.type = "text";
+  comment.placeholder = "Enter your comments here";
+  comment.className = "comments";
+  comment.value = comments;
+  // comment.id = "comments";
+
+  var editbutton = document.createElement('input');  //EDITBUTTON PROPERTIES
   editbutton.type = "button";
   editbutton.value = "Edit";
   editbutton.className = "edit";
 
 
-  function func() {
-    var x = this.value;
+
+  function func(x) {               //FUNCTION TO CHANGE COLOR BASED ON RATING
     rating.push(x);
+    myobj.rat = x;
     var yg = Math.ceil(63.75 * (x - 1));
     var yr = Math.ceil(-63.75 * (x - 5));
     var ygd = yg.toString(16);
@@ -91,87 +110,147 @@ function add() {
     if (yrd.length == 1) yrd = '0' + yrd;
     var mystring = '#' + yrd.toUpperCase() + ygd.toUpperCase() + '00';
     div.style.background = mystring;
+
     var pos = mentees.indexOf(div);
     rating[pos] = x;
+
   }
+  for(i=0;i<5;i++)
+  radio[i].addEventListener("click", function(){ func(this.value)});
 
 
-  for (i = 0; i < 5; i++)
-    radio[i].addEventListener('click', func);
+
+
   var status = document.createElement('input');
   status.type = "radio";
   status.style.display = "none";
   status.name = "status";
   status.className = "status";
-  status.checked = false;
+  status.checked = true;
 
-  div.appendChild(node);
+
+
+  div.appendChild(node);                    //APPENDING CHILDREN TO LIST
   div.appendChild(para);
+  div.appendChild(status);
 
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < 5; i++) {               //Radio buttons and labels
     div.appendChild(radio[i]);
     div.appendChild(label[i]);
   }
-  list.appendChild(status);
 
-  div.appendChild(comments);
-  div.appendChild(button);
+
+
+  div.appendChild(deletebutton);
   div.appendChild(lockbutton);
   div.appendChild(editbutton);
-  div.appendChild(status);
+
   list.appendChild(div);
-  mentees.push(div);
+  div.appendChild(comment);
+  mentees.push(div);                   //pushing the div to metees for sorting
 
 
 
 
-  button.onclick = function() {
+  deletebutton.onclick = function() {                     //Delete function
     var index = mentees.indexOf(div);
     div.remove();
     mentees.splice(index, 1);
     rating.splice(index, 1);
+    objarray.splice(index,1);
+    console.log(objarray.length);
 
   }
-  lockbutton.onclick = function() { //LOCK BUTTON onclick
-    comments.disabled = true;
+  function lock() { //LOCK BUTTON onclick
+    comment.disabled = true;
     for (m = 0; m < 5; m++)
       radio[m].disabled = true;
     node.disabled = true;
+    myobj.lockstatus = true;
+    myobj.comments = comment.value;
+    myobj.name = node.value;
   }
 
-  editbutton.onclick = function() {
-    comments.disabled = false;
+  function edit() {
+    comment.disabled = false;
     for (m = 0; m < 5; m++)
       radio[m].disabled = false;
     node.disabled = false;
+    myobj.lockstatus = false;
 
   }
+  if(lockstatus==true) lock();
+  else edit();
+
+  editbutton.addEventListener("click", edit);
+  lockbutton.addEventListener("click", lock);
 
 
 
 
+myobj.name = node.value;                //EDITING OBJECT PARAMETERS
+myobj.comments = comment.value;
+var index = mentees.indexOf(div);
+objarray[index] = myobj;
+
+// div.tabIndex = "0";
+
+// div.onfocus = function(){
+//   comment.style.display = "block";
+// }
+//
+// div.onblur = function(){
+//   comment.style.display = "none";
+// }
+div.onclick = function(){
+  status.checked = true;
+
+}
+div.tabIndex = "0";
+status.tabIndex = "0";
+ function expandcollapse(){
+
+  if(this.checked==true)
+  {
+    alert(status.checked);
+    div.style.marginBottom = "1%";
+    div.style.gridTemplateRows =  "1fr 0.5fr 0.5fr 1fr 1fr 1fr 1fr 1fr";
+    comment.style.display = "block";
+  }
+
+  if(this.checked==false)
+{
+    div.style.marginBottom = "1%";
+    div.style.gridTemplateRows =  "1fr 0.5fr 0.5fr 1fr";
+    comment.style.display = "none";
+  }
+}
 
 
-} //END OF ADD FUNCTION
+
+status.onclick = expandcollapse();
 
 
-function save() {                 //SAVE FUNCTION
+}                                            //END OF ADD FUNCTION
 
 
-    localStorage.clear();
+function save() {                                      //SAVE FUNCTION
       localStorage.setItem("flag", "1");
-  var ment = [];
-  for (i = 0; i < mentees.length; i++) {
-    ment[i] = mentees[i];
+      var obj2 = [];
 
-      JSON.stringify(ment[i]);
-      localStorage.setItem(i, JSON.stringify(ment[i]));
-      console.log(localStorage.getItem(i));
-
-  }
-  localStorage.setItem("length", mentees.length);
+  for (i = 0; i < objarray.length; i++) {
+    obj2[i] = objarray[i];
+    JSON.stringify(obj2[i]);
+     localStorage.setItem(i, JSON.stringify(obj2[i]));
 
 
+
+
+
+
+
+}
+  localStorage.setItem("length", objarray.length);
 
 }
 
@@ -191,6 +270,9 @@ function sort() {
         temp = rating[i];
         rating[i] = rating[k];
         rating[k] = temp;
+        var temp = objarray[i];
+        objarray[i] = objarray[k];
+        objarray[k] = temp;
       }
     }
   }
@@ -204,5 +286,6 @@ function sort() {
     mentees[i] = mentees2[i];
 
   }
+
 
 }
